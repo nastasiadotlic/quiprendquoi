2924bc9c1695a7a3ce78b151988d10e74e3a70ff
nastasiadotlic <nastasiadotlic@gmail.com>
2020-03-16 12:31:04 +0100

    title

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv').config();
 app.set('view engine', 'pug');
 
 app.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', { title: 'Nastasia - Qui prend quoi ?' });
 });
 
 app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
