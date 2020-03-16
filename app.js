e4facbfb4ba5254d00133d196429078b27eeb1bd
nastasiadotlic <nastasiadotlic@gmail.com>
2020-03-16 12:10:59 +0100

    env

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const dotenv = require('dotenv').config();
 
 app.get('/', function(req, res) {
   res.send('Hello');
 });
 
-app.listen(port, () => console.log(`Front app listening on port ${port}!`));
+app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
