d5c01f465ed2548bf60352185463ab8602e6073c
nastasiadotlic <nastasiadotlic@gmail.com>
2020-03-18 11:39:14 +0100

    copy

--- scripts/clipboard.js
+++ scripts/clipboard.js
@@ -1,11 +1,24 @@
 if (navigator.clipboard) {
-  console.log("Support du presse papier")
+  document.querySelectorAll('[data-clipboard]').forEach(($clipboardEl) => {
+    const $button = document.createElement('button');
+    $button.innerHTML = 'Copier';
+    $clipboardEl.parentNode.append($button);
+
+    $button.addEventListener(
+      'click',
+      copyToClipboard.bind(this, $clipboardEl, $button),
+    );
+  });
 } else {
-  console.warn("Pas de support")
+  console.log('Presse papier non supporté :(');
 }
 
-document.querySelectorAll('[data-clipboard]').forEach(($clipboardEl) => {
-  const $button = document.createElement('button');
-  $button.innerHTML = 'Copier';
-  $clipboardEl.parentNode.append($button);
-});
+function copyToClipboard($clipboardEl, $button) {
+  navigator.clipboard
+    .writeText($clipboardEl.getAttribute('data-clipboard'))
+    .then(() => {
+      $button.innerHTML = 'Copié !';
+      setTimeout(() => ($button.innerHTML = 'Copier'), 2000);
+    })
+    .catch((err) => console.warn(err));
+}
