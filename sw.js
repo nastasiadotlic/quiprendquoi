ae3942dca1656f14bddae457f1a966d6da9bc427
nastasiadotlic <nastasiadotlic@gmail.com>
2020-03-19 09:41:20 +0100

    reload sw

--- sw.js
+++ sw.js
@@ -1,2 +1,6 @@
-const name = 'nasti';
-console.log(`Hello ${name}`);
+// const name = 'nasti';
+// console.log(`Hello ${name}`);
+
+addEventListener('install', (event) => {
+  console.log('Hello from the service worker')
+});
